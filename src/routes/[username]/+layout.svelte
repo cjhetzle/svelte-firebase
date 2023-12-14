c8dff16200851a868a88c8dec388d4be2311282c
Cameron Hetzler <cjhetz@gmail.com>
2023-12-14 18:54:48 -0500

    Something

---
+++ src/routes/[username]/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { userData } from '$lib/firebase';
+	$: href = `/${$userData?.username}/`;
+</script>
+
+<main>
+	<div class="flex justify-between w-full navbar bg-base-100">
+		<div class="justify-start">
+			<a class="btn btn-ghost text-xl" {href}>daisyUI</a>
+		</div>
+		<div class="justify-end">
+			<a class="btn btn-ghost" href="/login">Login</a>
+		</div>
+	</div>
+</main>
+<slot />
